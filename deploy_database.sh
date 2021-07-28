e0058155a9a682ec960d2cfda371cfff15557c16
Martin <martinpfatrisch@gmail.com>
2021-07-28 21:03:10 +0200

    Wrote environment variables into external file

--- deploy_database.sh
+++ deploy_database.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+# File for all environment variables
+. "/srv/Nextcloud/Variables.env"
+
 podman run \
   --name nextcloud-db \
   --detach \
-  --env MYSQL_DATABASE=nextcloud \
-  --env MYSQL_PASSWORD=liebeshoehle \
-  --env MYSQL_ROOT_PASSWORD=liebeshoehle \
+  --env MYSQL_DATABASE=$DATABASE_NAME \
+  --env MYSQL_PASSWORD=$DATABASE_PASSWORD \
+  --env MYSQL_ROOT_PASSWORD=$DATABASE_ROOT_PASSWORD \
   --volume /srv/Nextcloud/nextcloud-db:/var/lib/mysql \
   --pod nextcloud-pod \
   --restart on-failure \
