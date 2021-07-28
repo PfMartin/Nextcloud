9fdcd868b72cacb874467fd8ac384ed91852e61d
Martin <martinpfatrisch@gmail.com>
2021-07-28 19:41:34 +0200

    Added nginx to the setup script

--- setup.sh
+++ setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-mkdir -p /srv/Nextcloud/nextcloud-app
-mkdir -p /srv/Nextcloud/nextcloud-data
-mkdir -p /srv/Nextcloud/nextcloud-db
+podport="8080"
+directoryroot="/srv/Nextcloud"
 
+mkdir -p ${directoryroot}/nextcloud-app
+mkdir -p ${directoryroot}/nextcloud-data
+mkdir -p ${directoryroot}/nextcloud-db
+mkdir -p ${directoryroot}/nginx
+
+cp ./nginx/* ${directoryroot}/nginx
 
 podman pod ps | grep nextcloud-pod
 if [[ $? == 0 ]]; then
   podman pod stop nextcloud-pod && podman pod rm -f nextcloud-pod
 fi
 
-podman pod create --name nextcloud-pod --publish 8080:80
+podman pod create --name nextcloud-pod --publish ${podport}:80
