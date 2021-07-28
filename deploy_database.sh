2d56a00eaf343a50b35a0ce073527feb7d3a1562
Martin <martinpfatrisch@gmail.com>
2021-07-28 21:55:56 +0200

    Added variables

--- deploy_database.sh
+++ deploy_database.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+. "/srv/Variables.env"
+
 podman run \
   --name nextcloud-db \
   --detach \
-  --env MYSQL_DATABASE=nextcloud \
-  --env MYSQL_PASSWORD=liebeshoehle \
-  --env MYSQL_ROOT_PASSWORD=liebeshoehle \
+  --env MYSQL_DATABASE=$DATABASE_NAME \
+  --env MYSQL_PASSWORD=$DATABASE_PASSWORD \
+  --env MYSQL_ROOT_PASSWORD=$DATABASE_PASSWORD_ROOT \
   --volume /srv/Nextcloud/nextcloud-db:/var/lib/mysql \
   --pod nextcloud-pod \
   --restart on-failure \
--- deploy_nextcloud.sh
+++ deploy_nextcloud.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+. "/srv/Variables.env"
+
 podman run \
   --name nextcloud \
   --detach \
   --env MYSQL_HOST=nextcloud-db.dns.podman \
-  --env MYSQL_DATABASE=nextcloud \
-  --env MYSQL_password=liebeshoehle \
-  --env NEXTCLOUD_ADMIN_USER=Martin \
-  --env NEXTCLOUD_ADMIN_PASSWORD=Jahnel01 \
+  --env MYSQL_DATABASE=$DATABASE_NAME \
+  --env MYSQL_password=$DATABASE_PASSWORD \
+  --env NEXTCLOUD_ADMIN_USER=$NEXTCLOUD_ADMIN \
+  --env NEXTCLOUD_ADMIN_PASSWORD=$NEXTCLOUD_PASSWORD \
   --volume /srv/Nextcloud/nextcloud-app:/var/www/html \
   --volume /srv/Nextcloud/nextcloud-data:/var/www/html/data \
   --restart on-failure \
