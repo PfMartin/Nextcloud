e476737bea77744f0794e39af08fe785da3aa4f4
Martin <martinpfatrisch@gmail.com>
2021-07-28 19:47:43 +0200

    Added config file and deployment script

--- deploy_nginx.sh
+++ deploy_nginx.sh
@@ -1,1 +1,9 @@
 #!/bin/bash
+
+podman run \
+  --name nextcloud-nginx \
+  --detach \
+  --volume /srv/Nextcloud/nginx:/etc/nginx/conf.d \
+  --restart on-failure \
+  --pod nextcloud-pod \
+  nginx
